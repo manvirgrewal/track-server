709ffb96c7e2287d9a2fb0c5a0e1ec6966920f2f
AliveAgain <15mg7@caslab.queensu.ca>
2021-07-13 16:02:41 -0700

    setting up mongo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+const mongoURI = 'mongodb+srv://admin:passwordpassword@cluster0.egztl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance :)');
+});
+
+mongoose.connection.on('error', (err)=> {
+    console.log('There was an error connecting to mongo instance :/', err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hi there');
