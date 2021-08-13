b313caf79b43e9d5b0b81798ee7f2fc878cbe63d
AliveAgain <15mg7@caslab.queensu.ca>
2021-08-12 17:06:46 -0700

    track server done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoURI = 'mongodb+srv://admin:passwordpassword@cluster0.egztl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useUnifiedTopology: true
 });
 
 mongoose.connection.on('connected', () => {
@@ -16,8 +28,8 @@ mongoose.connection.on('error', (err)=> {
     console.log('There was an error connecting to mongo instance :/', err);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hi there');
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
